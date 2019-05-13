76cf9917508715cd3cb9adb8508dfdd7a6e66ee1
Krzysztof Lewandowski <krzysztof.lewandowski@fastmail.fm>
2019-05-13 14:22:44 +0200

    Add files via upload

---
+++ provision_ansible.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Starting provisioning of Ansible..."
+
+# 64bit only
+#sudo rpm -Uvh http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm
+#sudo yum install net-tools
+
+sudo yum -y install ansible git
+
+
+
